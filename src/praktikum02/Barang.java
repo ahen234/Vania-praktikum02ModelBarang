20fc378d1acdf711512068b170d8766057021b3d
ahen234 <ahenduakali@gmail.com>
2026-09-25 11:29:31 +0700

    praktikum02

---
+++ src/praktikum02/Barang.java
@@ -0,0 +1,22 @@
+package praktikum02;
+
+public class Barang {
+    
+    private String kode;
+    private String nama;
+    private int stok;
+    private String keterangan;
+    
+    public Barang(String kode, String nama, int stok,String keterangan) {
+        setKode(kode);
+        setNama(nama);
+        setStok(stok);
+        setKeterangan(keterangan);
+    }
+    
+    public String getKode(){
+        return kode;
+    }
+    
+    public void
+}
